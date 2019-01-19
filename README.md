7610d97e41d3f883d7b96c2cf23a7ac7c9598bc5
Agentzhao <tan.hongzhao@dhs.sg>
2019-01-19 04:43:11 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# pydone
+# pydone Package
+
+# Made by Agentzhao and Revold
+
+# Version 1.4.0
+### Bug Fixes for Description
+
+# Version 1.3.0
+### Released all data structure algorithms
+
+# Version 1.2.0
+### Released all searching algorithms
+
+# Version 1.1.0
+### Released all sorting algorithms
+
+# Version 1.0.1
+### Released bubble sort algorithm
+### Bug Fixes
+
+# Version 1.0
+### Released pydone in beta
+### Tested revold()
+
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydone",
+    version="1.4.0",
+    author="AI lmao",
+    author_email="tan.hongzhao@dhs.sg",
+    description="A small python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tensorflow/models",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
